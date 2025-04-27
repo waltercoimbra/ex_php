b662cbd4f2bcc8ab20ce15310c38d1e4014b6a4d
WALTER COIMBRA CORDEIRO <waltercoim@gmail.com>
2025-04-27 19:27:39 -0300

    commiting

---
+++ modelo_php/exercicio.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        
+    ?>
+	<br>
+	<a href="javascript:history.go(-1)">Voltar</a>
+</div>
+</body>
+</html>
+ 
---
+++ modelo_php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <form method="get" action="exercicio.php">
+	Nome: <input type="text" name="nome" placeholder="digite seu nome"/>
+	<input type="submit" value="Enviar"/>
+	
+	</form>
+</div>
+</body>
+</html>
+ 
